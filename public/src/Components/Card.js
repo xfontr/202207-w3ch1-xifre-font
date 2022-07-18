92e0f2a5001aa953212a774bfa841d07ced3194d
Xifre Font <xifre.font.97@gmail.com>
2022-07-18 20:01:04 +0200

    Created app component

---
+++ public/src/Components/Card.js
@@ -0,0 +1,5 @@
+import Components from "../Components.js";
+
+class Card extends Components {}
+
+export default Card;
--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,7 @@
+import App from "./Components/App.js";
 import CardList from "./Components/CardList.js";
+
+new App(document.body);
 
 const app = document.querySelector(".app.container");
 
