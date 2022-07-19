ef5c27d4c0d11ddced59eaec963093f2c1d70e46
Xifre Font <xifre.font.97@gmail.com>
2022-07-19 21:34:27 +0200

    Created component file and structure

---
+++ public/src/Components/Communications/Communications.js
@@ -0,0 +1,9 @@
+import Components from "../../Components.js";
+
+class Communications extends Components {
+    constructor(parent) {
+        super(parent, "div", "comunications");
+    }
+}
+
+export default Communications;
