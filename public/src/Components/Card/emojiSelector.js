a727f9923f5fdfd9ad7513fd1587424adec0e4ec
Xifre Font <xifre.font.97@gmail.com>
2022-07-19 21:31:56 +0200

    Emoji added

--- public/src/Components/Card/emojiSelector.js
+++ public/src/Components/Card/emojiSelector.js
@@ -1,3 +1,26 @@
-const emojiSelector = () => {};
+import Asesor from "../../Characters/Asesor.js";
+import Escudero from "../../Characters/Escudero.js";
+import Luchador from "../../Characters/Luchador.js";
+import Rey from "../../Characters/Rey.js";
+
+const emojiSelector = (character) => {
+    if (character instanceof Rey) {
+        return "👑";
+    }
+
+    if (character instanceof Luchador) {
+        return "🗡";
+    }
+
+    if (character instanceof Asesor) {
+        return "🎓 ";
+    }
+
+    if (character instanceof Escudero) {
+        return "🛡";
+    }
+
+    return false;
+};
 
 export default emojiSelector;
